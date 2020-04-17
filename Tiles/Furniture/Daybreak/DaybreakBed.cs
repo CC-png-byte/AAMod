7bb593eefbf4b00504f34522487d4d8970c28b52
Alphakip <38961891+Alphakip@users.noreply.github.com>
2020-04-17 17:20:12 -0600

    e

--- Tiles/Furniture/Daybreak/DaybreakBed.cs
+++ Tiles/Furniture/Daybreak/DaybreakBed.cs
@@ -73,18 +73,5 @@ namespace AAMod.Tiles.Furniture.Daybreak
 			player.showItemIcon = true;
 			player.showItemIcon2 = mod.ItemType("Daybreak Bed");
 		}
-
-
-		public override void PostDraw(int i, int j, SpriteBatch spriteBatch)
-		{
-			Tile tile = Main.tile[i, j];
-			Vector2 zero = new Vector2(Main.offScreenRange, Main.offScreenRange);
-			if (Main.drawToScreen)
-			{
-				zero = Vector2.Zero;
-			}
-			int height = tile.frameY == 36 ? 18 : 16;
-			Main.spriteBatch.Draw(mod.GetTexture("Glowmasks/DaybreakBed_Glow"), new Vector2((i * 16) - (int)Main.screenPosition.X, (j * 16) - (int)Main.screenPosition.Y) + zero, new Rectangle(tile.frameX, tile.frameY, 16, height), Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
-		}
 	}
 }
--- Tiles/Furniture/Daybreak/DaybreakDresser.cs
+++ Tiles/Furniture/Daybreak/DaybreakDresser.cs
@@ -231,18 +231,5 @@ namespace AAMod.Tiles.Furniture.Daybreak
 			Item.NewItem(i * 16, j * 16, 48, 32, dresserDrop);
 			Chest.DestroyChest(i, j);
 		}
-
-
-		public override void PostDraw(int i, int j, SpriteBatch spriteBatch)
-		{
-			Tile tile = Main.tile[i, j];
-			Vector2 zero = new Vector2(Main.offScreenRange, Main.offScreenRange);
-			if (Main.drawToScreen)
-			{
-				zero = Vector2.Zero;
-			}
-			int height = tile.frameY == 36 ? 18 : 16;
-			Main.spriteBatch.Draw(mod.GetTexture("Glowmasks/DaybreakDresser_Glow"), new Vector2((i * 16) - (int)Main.screenPosition.X, (j * 16) - (int)Main.screenPosition.Y) + zero, new Rectangle(tile.frameX, tile.frameY, 16, height), Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
-		}
 	}
 }
